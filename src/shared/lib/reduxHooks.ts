8ef2e391c934064be893f1f8819ab0dbe14323d9
Nruzhzhaugash <clonn262@gmail.com>
2024-04-04 10:56:45 +0500

    test

---
+++ src/shared/lib/reduxHooks.ts
@@ -0,0 +1,6 @@
+"use client";
+import { AppDispatch, RootState } from "@/app/providers/store";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
